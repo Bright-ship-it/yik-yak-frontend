4d22a235bb58c12975b49ef8f9b11f0592d82656
{Bright-ship-it} <{wilbright.amugune@student.moringaschool.com}>
2022-09-11 23:21:09 +0300

    second commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log("Hello there")
+const postForm = document.getElementById("post-form")
+const postInput = document.getElementById("post-input")
+
+postForm.addEventListener("submit", submitPost)
+
+function submitPost(){
+    event.preventDefault()
+    console.log(postInput.value)
+    const li = document.createElement('li')
+
+    const p  = document.createElement('p')
+    p.innerText = postInput.value
+    li.appendChild(li)
+
+    const commentForm = document.createElement('form')
+    commentForm.innerHTML += `<input type="text"><input type="submit"`
+}
